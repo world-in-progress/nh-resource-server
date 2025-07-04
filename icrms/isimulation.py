2d31ae835b4a1fb79ed345437f6975e16bf7875c
YiFeng <2605948957@qq.com>
2025-07-04 15:33:57 +0800

    refactor: remove unused feature module import

--- icrms/isimulation.py
+++ icrms/isimulation.py
@@ -3,8 +3,6 @@ from enum import Enum
 from pydantic import BaseModel
 from typing import Union, Any
 
-from nh_resource_server.schemas import feature
-
 class CreateSimulationBody(BaseModel):
     name: str
     solution_name: str
